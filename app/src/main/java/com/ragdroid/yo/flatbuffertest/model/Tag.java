bb37e2a967bc741c733f26502a4dd333c58e9ce8
ragdroid <garima.my.way@gmail.com>
2015-08-20 16:03:30 +0530

    initial commit

---
+++ app/src/main/java/com/ragdroid/yo/flatbuffertest/model/Tag.java
@@ -0,0 +1,42 @@
+// automatically generated, do not modify
+
+package com.ragdroid.yo.flatbuffertest.model;
+
+import java.nio.*;
+import java.lang.*;
+import java.util.*;
+import com.google.flatbuffers.*;
+
+public final class Tag extends Table {
+  public static Tag getRootAsTag(ByteBuffer _bb) { return getRootAsTag(_bb, new Tag()); }
+  public static Tag getRootAsTag(ByteBuffer _bb, Tag obj) { _bb.order(ByteOrder.LITTLE_ENDIAN); return (obj.__init(_bb.getInt(_bb.position()) + _bb.position(), _bb)); }
+  public Tag __init(int _i, ByteBuffer _bb) { bb_pos = _i; bb = _bb; return this; }
+
+  public String id() { int o = __offset(4); return o != 0 ? __string(o + bb_pos) : null; }
+  public ByteBuffer idAsByteBuffer() { return __vector_as_bytebuffer(4, 1); }
+  public String name() { int o = __offset(6); return o != 0 ? __string(o + bb_pos) : null; }
+  public ByteBuffer nameAsByteBuffer() { return __vector_as_bytebuffer(6, 1); }
+  public String text() { int o = __offset(8); return o != 0 ? __string(o + bb_pos) : null; }
+  public ByteBuffer textAsByteBuffer() { return __vector_as_bytebuffer(8, 1); }
+
+  public static int createTag(FlatBufferBuilder builder,
+      int id,
+      int name,
+      int text) {
+    builder.startObject(3);
+    Tag.addText(builder, text);
+    Tag.addName(builder, name);
+    Tag.addId(builder, id);
+    return Tag.endTag(builder);
+  }
+
+  public static void startTag(FlatBufferBuilder builder) { builder.startObject(3); }
+  public static void addId(FlatBufferBuilder builder, int idOffset) { builder.addOffset(0, idOffset, 0); }
+  public static void addName(FlatBufferBuilder builder, int nameOffset) { builder.addOffset(1, nameOffset, 0); }
+  public static void addText(FlatBufferBuilder builder, int textOffset) { builder.addOffset(2, textOffset, 0); }
+  public static int endTag(FlatBufferBuilder builder) {
+    int o = builder.endObject();
+    return o;
+  }
+};
+
